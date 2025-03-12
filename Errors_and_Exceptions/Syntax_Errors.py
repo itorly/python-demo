9ea627d88195424ed875857de47006360bf42058
itorly <itorly@163.com>
2025-03-12 23:12:11 +0800

    Handling Exceptions

--- Errors_and_Exceptions/Syntax_Errors.py
+++ Errors_and_Exceptions/Syntax_Errors.py
@@ -5,3 +5,33 @@ while True:
         break
     except ValueError:
         print("Oops!  That was no valid number.  Try again...")
+
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
+
+try:
+    raise Exception('spam', 'eggs')
+except Exception as inst:
+    print(type(inst))    # the exception type
+    print(inst.args)     # arguments stored in .args
+    print(inst)          # __str__ allows args to be printed directly,
+                         # but may be overridden in exception subclasses
+    x, y = inst.args     # unpack args
+    print('x =', x)
+    print('y =', y)
