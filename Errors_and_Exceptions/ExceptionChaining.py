1ffe9e1a25683098718a9aa1ff5184ba7f4615c3
itorly <itorly@163.com>
2025-03-14 16:37:42 +0800

    Exception Chaining

---
+++ Errors_and_Exceptions/ExceptionChaining.py
@@ -0,0 +1,22 @@
+# 8.5. Exception Chaining
+# try:
+#     open("database.sqlite")
+# except OSError:
+#     raise RuntimeError("unable to handle error")
+
+# exc must be exception instance or None.
+# raise RuntimeError from exc
+
+def func():
+    raise ConnectionError
+
+try:
+    func()
+except ConnectionError as exc:
+    raise RuntimeError('Failed to open database') from exc
+
+
+try:
+    open('database.sqlite')
+except OSError:
+    raise RuntimeError from None
