625e50b997d4cd97d80a674a471c2f5bc21cbb0e
itorly <itorly@163.com>
2025-03-14 16:17:06 +0800

    Raising Exceptions

---
+++ Errors_and_Exceptions/RaisingExceptions.py
@@ -0,0 +1,10 @@
+# 8.4. Raising Exceptions
+raise NameError('HiThere')
+
+raise ValueError  # shorthand for 'raise ValueError()'
+
+try:
+    raise NameError('HiThere')
+except NameError:
+    print('An exception flew by!')
+    raise
