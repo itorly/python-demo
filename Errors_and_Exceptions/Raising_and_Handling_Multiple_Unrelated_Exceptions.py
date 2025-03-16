58153d01a266c769dcb381a9a0fcc87ad7b306e5
itorly <itorly@163.com>
2025-03-16 22:44:22 +0800

    Raising and Handling Multiple Unrelated Exceptions: except*

--- Errors_and_Exceptions/Raising_and_Handling_Multiple_Unrelated_Exceptions.py
+++ Errors_and_Exceptions/Raising_and_Handling_Multiple_Unrelated_Exceptions.py
@@ -1,13 +1,39 @@
 # 8.9. Raising and Handling Multiple Unrelated Exceptions
 # ExceptionGroup
-def f():
-    excs = [OSError('error 1'), SystemError('error 2')]
-    raise ExceptionGroup('there were problems', excs)
+# def f():
+#     excs = [OSError('error 1'), SystemError('error 2')]
+#     raise ExceptionGroup('there were problems', excs)
+#
+# # f()
+#
+#
+# try:
+#     f()
+# except Exception as e:
+#     print(f'caught {type(e)}: e')
 
-# f()
 
+
+# using except*
+def g():
+    raise ExceptionGroup(
+        "group1",
+        [
+            OSError(1),
+            SystemError(2),
+            ExceptionGroup(
+                "group2",
+                [
+                    OSError(3),
+                    RecursionError(4)
+                ]
+            )
+        ]
+    )
 
 try:
-    f()
-except Exception as e:
-    print(f'caught {type(e)}: e')
+    g()
+except* OSError as e:
+    print("There were OSErrors")
+except* SystemError as e:
+    print("There were SystemErrors")
