fad260c60c906a8a82ed316417b8c3092a3cdf79
itorly <itorly@163.com>
2025-03-16 22:20:01 +0800

    Raising and Handling Multiple Unrelated Exceptions: ExceptionGroup

---
+++ Errors_and_Exceptions/Raising_and_Handling_Multiple_Unrelated_Exceptions.py
@@ -0,0 +1,13 @@
+# 8.9. Raising and Handling Multiple Unrelated Exceptions
+# ExceptionGroup
+def f():
+    excs = [OSError('error 1'), SystemError('error 2')]
+    raise ExceptionGroup('there were problems', excs)
+
+# f()
+
+
+try:
+    f()
+except Exception as e:
+    print(f'caught {type(e)}: e')
