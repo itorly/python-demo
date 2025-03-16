0450862c8b86751dce7f7bc023410c76e4d8c1ec
itorly <itorly@163.com>
2025-03-16 23:54:03 +0800

    the built-in dir() and help() functions

--- Standard_Library/Operating_System_Interface.py
+++ Standard_Library/Operating_System_Interface.py
@@ -4,3 +4,8 @@ print(wd)
 os.chdir('/server/accesslogs')   # Change current working directory
 os.system('mkdir today')   # Run the command mkdir in the system shell
 
+# the built-in dir() and help() functions
+dir = dir(os)
+print(dir)
+
+help(os)
