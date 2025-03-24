83bc6705605db206ee5404a97603a9aeecbd9363
itorly <itorly@163.com>
2025-03-24 23:56:02 +0800

    Tools for Working with Lists: bisect

--- Standard_Library/Tools_for_Working_with_Lists.py
+++ Standard_Library/Tools_for_Working_with_Lists.py
@@ -1,14 +1,20 @@
 # 11.7. Tools for Working with Lists
-from array import array
-a = array('H', [4000, 10, 700, 22222])
-sum = sum(a)
-print(sum)
+# from array import array
+# a = array('H', [4000, 10, 700, 22222])
+# sum = sum(a)
+# print(sum)
+#
+# a_ = a[1:3]
+# print(a_)
+#
+#
+# from collections import deque
+# d = deque(["task1", "task2", "task3"])
+# d.append("task4")
+# print("Handling", d.popleft())
 
-a_ = a[1:3]
-print(a_)
 
-
-from collections import deque
-d = deque(["task1", "task2", "task3"])
-d.append("task4")
-print("Handling", d.popleft())
+import bisect
+scores = [(100, 'perl'), (200, 'tcl'), (400, 'lua'), (500, 'python')]
+bisect.insort(scores, (300, 'ruby'))
+print(scores)
