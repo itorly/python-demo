4fcdb2a67941c0d4eee7a7ad39f3e201aed489f7
itorly <itorly@163.com>
2025-03-24 23:58:40 +0800

    Tools for Working with Lists: heapq

--- Standard_Library/Tools_for_Working_with_Lists.py
+++ Standard_Library/Tools_for_Working_with_Lists.py
@@ -14,7 +14,15 @@
 # print("Handling", d.popleft())
 
 
-import bisect
-scores = [(100, 'perl'), (200, 'tcl'), (400, 'lua'), (500, 'python')]
-bisect.insort(scores, (300, 'ruby'))
-print(scores)
+# import bisect
+# scores = [(100, 'perl'), (200, 'tcl'), (400, 'lua'), (500, 'python')]
+# bisect.insort(scores, (300, 'ruby'))
+# print(scores)
+
+
+from heapq import heapify, heappop, heappush
+data = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
+heapify(data)                      # rearrange the list into heap order
+heappush(data, -5)                 # add a new entry
+heap_ = [heappop(data) for i in range(3)]  # fetch the three smallest entries
+print(heap_)
